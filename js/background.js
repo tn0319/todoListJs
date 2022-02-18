e9225d734c7ceda5dd461e867e8f5512a8841110
tn0319 <dtqe123@gmail.com>
2022-02-18 15:59:07 +0900

    경로 수정

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@ const images = ["0.jpg", "1.jpg", "2.png", "3.jpg"];
 
 const chosenImages = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
-bgImage.src = `/img/${chosenImages}`;
+bgImage.src = `./img/${chosenImages}`;
 
 document.body.appendChild(bgImage);
 
